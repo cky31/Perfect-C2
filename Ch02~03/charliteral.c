793fe3fe262e4839fa5157b5f6e1a3f20bae8120
cky31 <an2gen3@naver.com>
2016-10-03 21:10:21 +0900

    소스파일
    단원 폴더와 소스파일 추가

---
+++ Ch02~03/charliteral.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%Casic", 'B'); printf("%c", '\n');
+
+	char sq = '\n';
+
+	printf("BCPL\tB\tC\tjava\n");
+	printf("%C\7\n", '\a');
+	printf("%c자바언어'\n", sq);
+
+	printf("\"C언어\" 정말 재미있다!\n");
+
+	return 0;
+}
